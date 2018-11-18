cf80ed2a1ebb383a1ec6f0d8e692135569deffd7
Kelly <kellyzz9523@gmail.com>
2018-11-18 00:20:17 -0500

    Added guitar component
    Guitar.cpp
    Guitar.h

---
+++ include/Guitar.h
@@ -0,0 +1,45 @@
+//
+//  Guitar.h
+//  MiniStudioProject
+//
+//  Created by Kelly Zhang on 11/15/18.
+//
+
+//#ifndef Guitar_h
+//#define Guitar_h
+//
+//
+//#endif /* Guitar_h */
+
+extern YsSoundPlayer player;
+extern int windowW;
+extern int windowH;
+
+class GuitarChordBlock{
+public:
+    bool isPlaying=false;
+    void DrawChordBlock(int chordNum);
+    
+private:
+    double wid=24,hei=140,widActive=26,heiActive=140;
+    int colorR=153,colorG=153,colorB=255;
+    int colorActiveR=77,colorActiveG=255,colorActiveB=77;
+//    int numIter=0;
+};
+
+
+class Guitar:public Instrument{
+public:
+    void Load();
+    void Play();
+    void Draw();
+    void setKey(int keyVal);
+    Guitar();
+    //    ~Guitar();
+    
+private:
+    void DrawGuitarBackground() const;
+    void StopAll();
+    GuitarChordBlock blkEm,blkAm,blkF,blkC,blkD,blkG,blkDm;
+    YsSoundPlayer::SoundData Em,Am,F,C,D,G,Dm;
+};
---
+++ src/Guitar.cpp
@@ -0,0 +1,379 @@
+//
+//  Guitar.cpp
+//  MiniStudioProject
+//
+//  Created by Kelly Zhang on 11/11/18.
+//
+
+#include "Instrument.h"
+#include "Guitar.h"
+#include "yspng.h"
+#include <math.h>
+#include "ysglfontdata.h"
+
+void GuitarChordBlock::DrawChordBlock(int chordNum){
+    
+    double margin=15;
+    double x,y;
+    
+    x=(windowW-85)-chordNum*(margin+wid);
+    y=windowH/2;
+    
+//    if(numIter>24){
+//        isPlaying=false;
+//        numIter=0;
+//    }
+    
+    if (isPlaying==false){
+        // Draw shape with colorR, colorG and colorB & dimensions wid, hei.
+        glBegin(GL_QUADS);
+        glColor3ub(colorR,colorG,colorB);
+        glVertex2d(x-wid/2,y-hei/2);
+        glColor3ub(colorR,colorG,colorB);
+        glVertex2d(x+wid/2,y-hei/2);
+        glColor3ub(255,255,255);
+        glVertex2d(x+wid/2,y+hei/2);
+        glColor3ub(255,255,255);
+        glVertex2d(x-wid/2,y+hei/2);
+        glEnd();
+    }
+    else{
+        // Draw shape with colorActiveR, colorActiveG and colorActiveB & dimensions widActive, heiActive.
+        glBegin(GL_QUADS);
+        glColor3ub(colorActiveR,colorActiveG,colorActiveB);
+        glVertex2d(x-widActive/2,y-heiActive/2);
+        glColor3ub(colorActiveR,colorActiveG,colorActiveB);
+        glVertex2d(x+widActive/2,y-heiActive/2);
+        glColor3ub(255,255,255);
+        glVertex2d(x+widActive/2,y+heiActive/2);
+        glColor3ub(255,255,255);
+        glVertex2d(x-widActive/2,y+heiActive/2);
+        glEnd();
+    }
+  
+    // label chord names
+    glColor3f(0,0,0);
+    switch (chordNum) {
+        case 1:
+            glRasterPos2d(x-9,y-40);
+            YsGlDrawFontBitmap10x14("Em");
+            break;
+        case 2:
+            glRasterPos2d(x-9,y-40);
+            YsGlDrawFontBitmap10x14("Am");
+            break;
+        case 3:
+            glRasterPos2d(x-5,y-40);
+            YsGlDrawFontBitmap10x14("F");
+            break;
+        case 4:
+            glRasterPos2d(x-5,y-40);
+            YsGlDrawFontBitmap10x14("C");
+            break;
+        case 5:
+            glRasterPos2d(x-5,y-40);
+            YsGlDrawFontBitmap10x14("D");
+            break;
+        case 6:
+            glRasterPos2d(x-5,y-40);
+            YsGlDrawFontBitmap10x14("G");
+            break;
+        case 7:
+            glRasterPos2d(x-9,y-40);
+            YsGlDrawFontBitmap10x14("Dm");
+            break;
+    }
+//    numIter+=1;
+}
+
+void Guitar::Load(){
+}
+
+Guitar::Guitar(){
+    Load();
+}
+
+void Guitar::setKey(int keyVal){
+    key=keyVal;
+}
+
+void Guitar::StopAll(){
+    player.Stop(Em);
+    player.Stop(Am);
+    player.Stop(F);
+    player.Stop(C);
+    player.Stop(D);
+    player.Stop(G);
+    player.Stop(Dm);
+    
+    blkEm.isPlaying=false;
+    blkAm.isPlaying=false;
+    blkF.isPlaying=false;
+    blkC.isPlaying=false;
+    blkD.isPlaying=false;
+    blkG.isPlaying=false;
+    blkDm.isPlaying=false;
+}
+
+void Guitar::Play(){
+    auto isActiveEm=player.IsPlaying(Em);
+    auto isActiveAm=player.IsPlaying(Am);
+    auto isActiveF=player.IsPlaying(F);
+    auto isActiveC=player.IsPlaying(C);
+    auto isActiveD=player.IsPlaying(D);
+    auto isActiveG=player.IsPlaying(G);
+    auto isActiveDm=player.IsPlaying(Dm);
+
+    if(YSTRUE!=isActiveEm){
+        blkEm.isPlaying=false;
+        if(YSTRUE!=isActiveAm){
+            blkAm.isPlaying=false;
+            if(YSTRUE!=isActiveF){
+                blkF.isPlaying=false;
+                if(YSTRUE!=isActiveC){
+                    blkC.isPlaying=false;
+                    if(YSTRUE!=isActiveD){
+                        blkD.isPlaying=false;
+                        if(YSTRUE!=isActiveG){
+                            blkG.isPlaying=false;
+                            if(YSTRUE!=isActiveDm){
+                                blkDm.isPlaying=false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    switch (key) {
+        case FSKEY_F:
+            StopAll();
+            blkEm.isPlaying=true;
+            Em.LoadWav("Em.wav");
+            player.PlayOneShot(Em);
+            break;
+        case FSKEY_G:
+            StopAll();
+            blkAm.isPlaying=true;
+            Am.LoadWav("Am.wav");
+            player.PlayOneShot(Am);
+            break;
+        case FSKEY_H:
+            StopAll();
+            blkF.isPlaying=true;
+            F.LoadWav("F.wav");
+            player.PlayOneShot(F);
+            break;
+        case FSKEY_V:
+            StopAll();
+            blkC.isPlaying=true;
+            C.LoadWav("C.wav");
+            player.PlayOneShot(C);
+            break;
+        case FSKEY_B:
+            StopAll();
+            blkD.isPlaying=true;
+            D.LoadWav("D.wav");
+            player.PlayOneShot(D);
+            break;
+        case FSKEY_N:
+            StopAll();
+            blkG.isPlaying=true;
+            G.LoadWav("G.wav");
+            player.PlayOneShot(G);
+            break;
+        case FSKEY_M:
+            StopAll();
+            blkDm.isPlaying=true;
+            Dm.LoadWav("Dm.wav");
+            player.PlayOneShot(Dm);
+            break;
+        case FSKEY_T:
+            StopAll();
+            break;
+    }
+}
+
+void Guitar::DrawGuitarBackground() const{
+    // Draw guitar background elements
+    // draw background color
+//    glColor3ub(179,89,0);
+    glColor3ub(230,153,0);
+
+    glBegin(GL_QUADS);
+    glVertex2i(0,0);
+    glVertex2i(windowW,0);
+    glVertex2i(windowW,windowH);
+    glVertex2i(0,windowH);
+    glEnd();
+    
+    // draw guitar body
+    int cx = windowW/2+50;
+    int cy = windowH/2;
+    int num_segments=500;
+    glShadeModel(GL_SMOOTH);
+    
+    int r0_1=220;
+    glBegin(GL_TRIANGLE_FAN);
+//    glColor3ub(255,230,102);
+//    glColor3ub(230,153,0);
+    glColor3ub(255,179,25);
+    glVertex2f(cx, cy);
+    for(int i=0;i<num_segments;i++)
+    {
+        double theta=2.0*3.14159265*double(i)/double(num_segments);
+        double x=r0_1*cos(theta);
+        double y=r0_1*sin(theta);
+//        glColor3ub(230,153,0);
+        glColor3ub(179,59,0);
+        glVertex2f(x+cx, y+cy);
+    }
+    glEnd();
+    
+    cx = windowW/2+50;
+    cy = windowH/2;
+    
+    // draw center hole
+    int r1=95;
+    glColor3ub(0,0,0);
+    glBegin(GL_TRIANGLE_FAN);
+    for(int i=0;i<num_segments;i++)
+    {
+        double theta=2.0*3.14159265*double(i)/double(num_segments);
+        double x=r1*cos(theta);
+        double y=r1*sin(theta);
+        glVertex2f(x+cx, y+cy);
+    }
+    glEnd();
+    
+    // -------------------
+    int r2=100;
+    glColor3ub(0,0,0);
+    glBegin(GL_LINE_LOOP);
+    for(int i=0;i<num_segments;i++)
+    {
+        double theta=2.0*3.14159265*double(i)/double(num_segments);
+        double x=r2*cos(theta);
+        double y=r2*sin(theta);
+        glVertex2f(x+cx, y+cy);
+    }
+    glEnd();
+    
+    int r3=110;
+    glColor3ub(0,0,0);
+    glBegin(GL_LINE_LOOP);
+    for(int i=0;i<num_segments;i++)
+    {
+        double theta=2.0*3.14159265*double(i)/double(num_segments);
+        double x=r3*cos(theta);
+        double y=r3*sin(theta);
+        glVertex2f(x+cx, y+cy);
+    }
+    glEnd();
+    
+    // ------------------------------
+    int r4=55;
+    double length=5;
+    glBegin(GL_LINES);
+    for(int i=0;i<100;i++)
+    {
+        double theta=2.0*3.14159265*double(i)/double(100);
+        double x=r4*cos(theta);
+        double y=r4*sin(theta);
+        
+        glColor3ub(0,0,0);
+        glVertex2f(x+cx+r4*cos(theta), y+cy+r4*sin(theta));
+        glVertex2f(x+cx+(r4+length)*cos(theta), y+cy+(r4+length)*sin(theta));
+    }
+    glEnd();
+    
+    // -------------------------------
+    int r5=115;
+    glColor3ub(0,0,0);
+    glBegin(GL_LINE_LOOP);
+    for(int i=0;i<num_segments;i++)
+    {
+        double theta=2.0*3.14159265*double(i)/double(num_segments);
+        double x=r5*cos(theta);
+        double y=r5*sin(theta);
+        glVertex2f(x+cx, y+cy);
+    }
+    glEnd();
+    
+    // draw fretboard
+    double fretX = windowW/2;
+    double fretY = windowH/2;
+    double fretW = windowW;
+    double fretH = 120; // adjust fretboard half width
+    double fretY1 = fretY+fretH/4;
+    double fretY2 = fretY-fretH/4;
+
+    glBegin(GL_QUADS);
+    glColor3ub(102,34,0);
+    glVertex2d(fretX-fretW/2,fretY1-fretH/4);
+    glColor3ub(102,34,0);
+    glVertex2d(fretX+fretW/2,fretY1-fretH/4);
+    glColor3ub(0,0,0);
+    glVertex2d(fretX+fretW/2,fretY1+fretH/4);
+    glColor3ub(0,0,0);
+    glVertex2d(fretX-fretW/2,fretY1+fretH/4);
+    glEnd();
+    
+    glBegin(GL_QUADS);
+    glColor3ub(0,0,0);
+    glVertex2d(fretX-fretW/2,fretY2-fretH/4);
+    glColor3ub(0,0,0);
+    glVertex2d(fretX+fretW/2,fretY2-fretH/4);
+    glColor3ub(102,34,0);
+    glVertex2d(fretX+fretW/2,fretY2+fretH/4);
+    glColor3ub(102,34,0);
+    glVertex2d(fretX-fretW/2,fretY2+fretH/4);
+    glEnd();
+    
+    // draw six uitar strings
+    double stringX=windowW/2;;
+    double stringSpc=18;
+    double stringY = windowH/2+45;
+    double stringW = windowW;
+    double stringH=6;
+    
+    for(int i=0;i<6;i++){
+        double stringY1 = stringY+stringH/4;
+        double stringY2 = stringY-stringH/4;
+        
+        glBegin(GL_QUADS);
+        glColor3ub(255,255,255);
+        glVertex2d(stringX-stringW/2,stringY1-i*stringSpc-stringH/4);
+        glColor3ub(255,255,255);
+        glVertex2d(stringX+stringW/2,stringY1-i*stringSpc-stringH/4);
+        glColor3ub(0,0,0);
+        glVertex2d(stringX+stringW/2,stringY1-i*stringSpc+stringH/4);
+        glColor3ub(0,0,0);
+        glVertex2d(stringX-stringW/2,stringY1-i*stringSpc+stringH/4);
+        glEnd();
+        
+        glBegin(GL_QUADS);
+        glColor3ub(0,0,0);
+        glVertex2d(stringX-stringW/2,stringY2-i*stringSpc-stringH/4);
+        glColor3ub(0,0,0);
+        glVertex2d(stringX+stringW/2,stringY2-i*stringSpc-stringH/4);
+        glColor3ub(255,255,255);
+        glVertex2d(stringX+stringW/2,stringY2-i*stringSpc+stringH/4);
+        glColor3ub(255,255,255);
+        glVertex2d(stringX-stringW/2,stringY2-i*stringSpc+stringH/4);
+        glEnd();
+    }
+}
+
+void Guitar::Draw(){
+    DrawGuitarBackground();
+    blkEm.DrawChordBlock(1);
+    blkAm.DrawChordBlock(2);
+    blkF.DrawChordBlock(3);
+    blkC.DrawChordBlock(4);
+    blkD.DrawChordBlock(5);
+    blkG.DrawChordBlock(6);
+    blkDm.DrawChordBlock(7);
+}
+
