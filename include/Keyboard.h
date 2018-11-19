951dc8ad2e31aa69af6396ce30a9934b70cdc2fe
adrian <heyadrianliu@hotmail.com>
2018-11-18 21:50:54 -0500

    Keyboard, add draw function

--- include/Keyboard.h
+++ include/Keyboard.h
@@ -1,13 +1,16 @@
 #pragma once
 #include "Instrument.h"
 extern YsSoundPlayer player;
+
 class Keyboard : public Instrument{
 public:
 	Keyboard();
-	void load();
-	void draw();
-	void play();
+	void Load();
+	void Draw();
+	void Play();
 	void setKey(int key);
 private:
-	YsSoundPlayer::SoundData data0, data1, data2;
+	//YsSoundPlayer::SoundData data0, data1, data2;
+	vector<shared_ptr<YsSoundPlayer::SoundData>> sounddataList_;
+	int top_, left_, keyW_, keyH_;
 };
